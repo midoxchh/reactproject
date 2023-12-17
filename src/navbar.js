6dbc66a9186fa01bf76fc3a49970eab33e0db212
mehdi_lghzal <mohamedmehdichahboune@gmail.com>
2023-12-18 00:53:47 +0100

    creat navbar

---
+++ src/navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./navbar.css"
+function Navbar(){
+    
+    return(
+            <div>
+                <nav>
+                    <div id="logo-nav">3allah </div>
+                    <div>
+                        <ul id="navlist">
+                            <li>home</li>
+                            <li>product</li>
+                            <li>contact</li>
+                        </ul>
+                    </div>
+                    <div id="search-bar">
+                        <input type="text" />
+                        <button><i class="bi bi-search"></i></button>
+                        
+
+                        
+                    </div>
+                </nav>
+            </div>
+    );
+}
+export default Navbar ;
